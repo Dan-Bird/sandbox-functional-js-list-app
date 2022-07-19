dd60396c51fc195c3b2ec239830afb5e09a0a2a8
Daniel <info@daniel-bird.co.uk>
2022-07-19 14:01:51 +0100

    Composition with Pipe

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hello');
+const elem = tag => document.createElement(tag);
+const text = content => document.createTextNode(content);
+
+const append = R.curry(function (node, element) {
+  element.appendChild(node);
+  return element;
+});
+
+const attr = R.curry(function (attributeName, attributeValue, element) {
+  element.setAttribute(attributeName, attributeValue);
+  return element;
+});
+
+const message = content =>
+  R.pipe(attr('data-message', content), append(text(content)))(elem('div'));
+
+document.body.appendChild(message('example message'));
