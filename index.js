8e58db9225323d0dcbfe6bfc2c92c105fa10ffeb
Daniel <info@daniel-bird.co.uk>
2022-07-19 14:17:42 +0100

    Batch messages into single container

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const app = (state, output) => {
-  const appendFns = state.map((content, index) => {
-    return append(message(content, index));
-  });
-
-  R.pipe(...appendFns)(output);
+  append(view(state), output);
 };
 
 const message = (content, index) =>
   R.pipe(attr('data-index', index), append(text(content)))(elem('div'));
+
+const view = state => {
+  const appendFns = state.map((content, index) => {
+    return append(message(content, index));
+  });
+
+  return R.pipe(...appendFns)(elem('div'));
+};
 
 app(
   Object.freeze(['first message', 'second message']),
